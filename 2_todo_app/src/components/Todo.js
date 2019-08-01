5b55414a6951f70c3707db9eabc4a44e0f595ab6
uriyang <girlstoryk@gmail.com>
2019-08-01 21:02:25 +0900

    4_change state
    - todo's change state
    - warning 아직 남아있다..

--- 2_todo_app/src/components/Todo.js
+++ 2_todo_app/src/components/Todo.js
@@ -1,25 +1,50 @@
 import React, { Component } from 'react';
 
 class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [
+        {
+          label: 'Pay Bills',
+          mode: 'view',
+        },
+        {
+          label: 'Go Shopping',
+          mode: 'edit',
+        },
+      ]
+    }
+  }
+  
   render() {
+    const { list } = this.state;
+
+    const changeStatus = (i) => {
+      const tempList = this.state.list;
+      tempList[i].mode = tempList[i].mode === 'view' ? 'edit' : 'view';
+      this.setState({
+        list: tempList
+      })
+    }
+
     return (
       <div>
         <h3>Todo</h3>
         <ul id="incomplete-tasks">
-          <li>
-            <input type="checkbox" />
-            <label>Pay Bills</label>
-            <input type="text" />
-            <button class="edit">Edit</button>
-            <button class="delete">Delete</button>
-          </li>
-          <li class="editMode">
-            <input type="checkbox" />
-            <label>Go Shopping</label>
-            <input type="text" value="Go Shopping" />
-            <button class="edit">Edit</button>
-            <button class="delete">Delete</button>
-          </li>
+          {
+            list.map((item, i) => {
+              return (
+                <li className={item.mode === 'edit' ? "editMode" : null}>
+                  <input type="checkbox" />
+                  <label>{item.label}</label>
+                  <input type="text" value={item.label ? item.label : null} />
+                  <button class="edit" onClick={() => changeStatus(i)}>Edit</button>
+                  <button class="delete">Delete</button>
+                </li>
+              );
+            })
+          }
         </ul>
       </div>
     );
