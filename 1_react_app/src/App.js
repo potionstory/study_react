ff62550c80e8a1db2cb427389aac0dbb759e7459
uriyang <girlstoryk@gmail.com>
2019-08-01 15:33:52 +0900

    feat: lesson_2~3
    - lesson_2~3

--- 1_react_app/src/App.js
+++ 1_react_app/src/App.js
@@ -1,12 +1,71 @@
-import React from 'react';
-// import logo from './logo.svg';
+import React, { Component } from 'react';
+import TOC from "./components/TOC";
+import Content from './components/Content';
+import Subject from "./components/Subject";
 
-function App() {
-  return (
-    <div className="App">
-      Hello, React!!
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: 'welcome',
+      selected_content_id: 2,
+      subject: { title: 'WEB', sub: 'world wide web!' },
+      welcome: {
+        title: 'Welcome',
+        desc: 'Hello, React!!'
+      },
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ],
+      content: { title: 'HTML', desc: 'HTML is HyperText Markup Language.' }
+    }
+  }
+  
+  render() {
+    let _title, _desc = null;
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read') {
+      let i = 0;
+      while (i < this.state.contents.length) {
+        let data = this.state.contents[i];
+
+        if (data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
+    }
+
+    return (
+      <div className="App">
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub} 
+          onChangePage={function() {
+            alert('hihihi');
+          }.bind(this)}
+        />
+        <TOC
+          onChangePage={function(id) {
+            this.setState({
+              mode: 'read',
+              selected_content_id: Number(id)
+            });
+          }.bind(this)}
+          data={this.state.contents} 
+        />
+        <Content
+          title={_title}
+          desc={_desc} />
+      </div>
+    );
+  }
 }
 
 export default App;
