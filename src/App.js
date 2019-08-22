d8460b3f0f22ff160fc6063a18d4fbb695c7c076
Potion Story <potionstory@gmail.com>
2019-08-22 23:12:06 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,72 +1,31 @@
 import React, { Component } from 'react';
-import AppChild from './AppChild';
+import PotionApp from './PotionApp';
+import LkelApp from './LkelApp';
+import MiniApp from './MiniApp';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0,
-      name: '',
-      age: 0,
-      member:[]
-    };
-  }
-
-  handleChange = (e) => {
-    const { name, value } = e.target;
-    this.setState({
-      [name]: value
-    });
-  };
-
-  handleCreate = () => {
-    this.setState({
-      count: ++this.state.count,
-      member: [
-        ...this.state.member,
-        {
-          id: this.state.count,
-          name: this.state.name,
-          age: this.state.age
-        }
-      ]
-    });
-  };
-
-  handleRemove = (i) => {
-    this.setState({
-      member: [
-        ...this.state.member.slice(0, i),
-        ...this.state.member.slice(i + 1, this.state.member.length)
-      ]
-    });
+  state = {
+    user: 1,
   };
 
   render() {
+
+    let component;
+    if(this.state.user === 1){
+      component = <PotionApp />;
+    } else if (this.state.user === 2) {
+      component = <LkelApp />;
+    } else {
+      component = <MiniApp />;
+    }
+
     return (
-      <>
-        <label htmlFor="id_name">이름</label>
-        <input type="text" id="id_name" name="name" onChange={this.handleChange} />
-        <br />
-        <label htmlFor="id_age">나이</label>
-        <input type="text" id="id_age" name="age" onChange={this.handleChange} />
-        <br />
-        <button type="button" onClick={this.handleCreate}>입력</button>
-        <div>
-          {this.state.member.map((v, i) => {
-            return (
-              <div key={v.id}>
-                <span>{v.name}</span>
-                <span>{v.age}</span>
-                <button type="button" onClick={() => this.handleRemove(i)}>삭제</button>
-              </div>
-            )
-          })}
-        </div>
-      </>
+      <div>
+        {component}
+      </div>
     );
-  };
+  }
 }
 
 export default App;
---
+++ src/PotionApp.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import AppChild from './AppChild';
+
+class PotionApp extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      name: '',
+      age: 0,
+      member:[]
+    };
+  }
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleCreate = () => {
+    this.setState({
+      count: ++this.state.count,
+      member: [
+        ...this.state.member,
+        {
+          id: this.state.count,
+          name: this.state.name,
+          age: this.state.age
+        }
+      ]
+    });
+  };
+
+  handleRemove = (i) => {
+    this.setState({
+      member: [
+        ...this.state.member.slice(0, i),
+        ...this.state.member.slice(i + 1, this.state.member.length)
+      ]
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <label htmlFor="id_name">이름</label>
+        <input type="text" id="id_name" name="name" onChange={this.handleChange} />
+        <br />
+        <label htmlFor="id_age">나이</label>
+        <input type="text" id="id_age" name="age" onChange={this.handleChange} />
+        <br />
+        <button type="button" onClick={this.handleCreate}>입력</button>
+        <div>
+          {this.state.member.map((v, i) => {
+            return (
+              <div key={v.id}>
+                <span>{v.name}</span>
+                <span>{v.age}</span>
+                <button type="button" onClick={() => this.handleRemove(i)}>삭제</button>
+              </div>
+            )
+          })}
+        </div>
+      </>
+    );
+  };
+}
+
+export default PotionApp;
